6ffd647ad0bb440d48bb203c9722058631cba5c0
philanc <philanc@gmail.com>
2016-08-27 20:50:30 -0400

    improved smoketest

--- smoketest.lua
+++ smoketest.lua
@@ -12,32 +12,46 @@ local pt, et, dt  -- plain text, encrypted text, decrypted text
 
 pt = ("a"):rep(3)
 
-local k = ("k"):rep(32) 
-local n = ("n"):rep(24)
+local k = ("k"):rep(32)  -- encryption key (for secretbox)
+local n = ("n"):rep(24)  -- nonce (should be different for each encryption)
 
--- box()
-local apk, ask = nacl.box_keypair() --alice
-local bpk, bsk = nacl.box_keypair() --bob
-et = nacl.box(pt, n, bpk, ask) --alice encrypts for bob
+-- box() -- 
+-- generate a key pair for Alice
+local apk, ask = nacl.box_keypair()  -- public key, secret key
+
+-- generate a key pair for Bob
+local bpk, bsk = nacl.box_keypair()  -- public key, secret key
+
+--Alice encrypts for Bob with Bob public key and her secret key
+et = nacl.box(pt, n, bpk, ask) 
+
+-- the encrypted text include a Poly1305 MAC (16 bytes)
 assert(#et == #pt + 16)
+
+-- Bob decrypts the message with  Alice public key and his secret key
 dt = nacl.box_open(et, n, apk, bsk) -- bob decrypts
 assert(dt == pt)
 
--- secretbox()
+-- secretbox() -- authenticated encryption (Salsa20 and Poly1305 MAC)
+
+-- encrypt the plain text
 et = nacl.secretbox(pt, n, k)
+
+-- decrypt the encrypted text (the Poly1305 MAC is checked)
 dt, msg = nacl.secretbox_open(et, n, k)
 assert(dt == pt)
--- encrypt empty text
+
+-- check secretbox() works with an empty text
 local ez = nacl.secretbox("", n, k)
 local dz, msg = nacl.secretbox_open(ez, n, k)
 assert(#ez == 16 and #dz == 0)
 
--- stream, stream_xor
+-- stream, stream_xor -- this exposes the raw Salsa20 algorithm
 local s3 = nacl.stream(3, n, k)
 local e3 = nacl.stream_xor("\0\0\0", n, k)
 assert(s3 == e3)
 
--- hash a.k.a. sha512
+-- hash a.k.a. sha512 - check the simplest test vector
 local s, h
 s = "abc"
 h = "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a" 
